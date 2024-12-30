9aea1235d6bd466002991ed848d596f53585ceaf
Prabhanjana-M-P <prabhanjanamp9@gmail.com>
2024-12-30 20:34:35 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import'./App.css';
+import Photos from './components/Photos';
+import Favourites from './components/Favourites';
+import { BrowserRouter ,Routes,  Route , Link } from 'react-router-dom';
+
+
+function App() {
+  return(
+    <BrowserRouter>
+      <div>
+        <nav className="navbar">
+          <div className="navbar_logo">
+            <h1>Fotoflix</h1>
+          </div>
+          <div className="search_input">
+            <input type="text" placeholder='Search for Photos..' />
+          </div>
+          <div className="navbar_links">
+            <Link to="/">HOME</Link>
+            <Link to="/favourites">Favourites</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Photos/>}/>
+          <Route path='/favourites' element={<Favourites/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App
